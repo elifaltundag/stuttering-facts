e9040fabe388fc0607fe0b732f5592e5b1ccc555
elifaltundag <elif.altundag.x@gmail.com>
2022-07-03 14:57:07 +0300

    Just transition text for now

--- main.js
+++ main.js
@@ -6,20 +6,8 @@ console.log(factText);
 
 generatorButton.addEventListener("click", () => {
     const factTextState = factText.dataset.state;
-    const btnPosition = generatorButton.dataset.position;
-
+    
     if (factTextState === "initial") {
         factText.dataset.state = "expanded";
     }
-
-    if (btnPosition === "initial") {
-        console.log("whyyy")
-        generatorButton.dataset.position = "is-pushing";
-        setTimeout(() => {
-            generatorButton.dataset.position = "pushed"
-        }, 400);
-    }
-
-    
-
 })
